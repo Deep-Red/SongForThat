05abcfd569c9bd2480401fb64f4eea952cbdc122
Deep Red <0k5786@gmail.com>
2017-11-17 09:44:57 +0100

    Ran db:migrate to drop approvals and disapprovals from Taggings model. Vote model makes them unneccessary.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171116131830) do
+ActiveRecord::Schema.define(version: 20171117082838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,8 +38,6 @@ ActiveRecord::Schema.define(version: 20171116131830) do
     t.bigint "song_id"
     t.bigint "tag_id"
     t.bigint "created_by_id"
-    t.integer "approvals", default: 0
-    t.integer "disapprovals", default: 0
     t.decimal "score", default: "0.0"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
