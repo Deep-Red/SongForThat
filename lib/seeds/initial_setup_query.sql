592feb6a1f27b5d1a35c16a7a905ea665e4cd032
Deep Red <0k5786@gmail.com>
2017-12-15 11:01:30 +0100

    New query. Skips previously entered (title, artist) combinations, and uses an outer join so that songs without an associated 'work' entry will be available.

--- lib/seeds/initial_setup_query.sql
+++ lib/seeds/initial_setup_query.sql
@@ -7,3 +7,17 @@ INNER JOIN musicbrainz.artist_credit ON (musicbrainz.recording.artist_credit = m
 INNER JOIN musicbrainz.l_recording_work ON (musicbrainz.recording.id = musicbrainz.l_recording_work.entity0)
 INNER JOIN musicbrainz.work ON (musicbrainz.l_recording_work.entity1 = musicbrainz.work.id)
 ;') AS a(title citext, recording_id integer, work_id integer, artist citext, artist_id integer, created_at timestamp, updated_at timestamp, added_by_id bigint);
+
+
+
+
+INSERT INTO songs (title, mb_recording, mb_work, artist, mb_artist_credit, created_at, updated_at, added_by_id)
+SELECT DISTINCT ON (title, artist) a.*
+FROM dblink('dbname=musicbrainz_db', 'SELECT DISTINCT ON(musicbrainz.artist_credit.id, musicbrainz.recording.id) musicbrainz.recording.name, musicbrainz.recording.id, musicbrainz.work.id, musicbrainz.artist_credit.name, musicbrainz.artist_credit.id,
+current_timestamp, current_timestamp, 1
+FROM musicbrainz.recording
+INNER JOIN musicbrainz.artist_credit ON (musicbrainz.recording.artist_credit = musicbrainz.artist_credit.id)
+LEFT JOIN musicbrainz.l_recording_work ON (musicbrainz.recording.id = musicbrainz.l_recording_work.entity0)
+LEFT JOIN musicbrainz.work ON (musicbrainz.l_recording_work.entity1 = musicbrainz.work.id)
+;') AS a(title citext, recording_id integer, work_id integer, artist citext, artist_id integer, created_at timestamp, updated_at timestamp, added_by_id bigint)
+ON CONFLICT DO NOTHING;
